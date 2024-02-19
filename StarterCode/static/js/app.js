ebf26b0601975cffa525132bc62ead7f78b48fcb
peggz19 <147319657+peggz19@users.noreply.github.com>
2024-02-19 16:16:11 -0500

    coding done

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -6,12 +6,56 @@ d3.json(url).then(function(data) {
     var jsondata = data;
     // loading the data to take a peek at what's in our json file
     console.log(jsondata)
+      
+    // first I create an initializing function for the first id containing the 2 graphs and the Demograpich info
+    // this function will then be copied and pasted then modified
+    // I believe the function is not really necessary but just for the sake of not opening an empty page, I created it.
+    function init() {
+        // BAR CHART
+        let trace1 = [{
+            // we use reverse to put the chart in descending order
+            x: jsondata.samples[0].sample_values.slice(0,10).reverse(),
+            y: jsondata.samples[0].otu_ids.slice(0, 10).map(otuID => `OTU ${otuID}`).reverse(),
+            type : 'bar',
+            // changing the orientation of the bar chart to horizontal
+            orientation : 'h',
+            // creating a hovertext
+            text : jsondata.samples[0].otu_labels.slice(0,10).reverse(),
+            }];
+        // BUBBLE CHART
+        let trace2 =[{
+            x : jsondata.samples[0].otu_ids,
+            y : jsondata.samples[0].sample_values,
+            mode : 'markers',
+            marker : {
+                size : jsondata.samples[0].sample_values,
+                color : jsondata.samples[0].otu_ids
+            },
+            text : jsondata.samples[0].otu_labels
+            }];
+       
+        // Displaying the plot
+        Plotly.newPlot('bar',trace1)
+        Plotly.newPlot('bubble',trace2)
+
+        //Displaying the Demographic info A.K.A metadata
+        d3.select('#sample-metadata.card-body').append().html(`id : ${jsondata.metadata[0].id}<br>`)
+        d3.select('#sample-metadata.card-body').append().html(`ethnicity : ${jsondata.metadata[0].ethnicity}<br>`)
+        d3.select('#sample-metadata.card-body').append().html(`gender : ${jsondata.metadata[0].gender}<br>`)
+        d3.select('#sample-metadata.card-body').append().html(`age : ${jsondata.metadata[0].age}<br>`)
+        d3.select('#sample-metadata.card-body').append().html(`location : ${jsondata.metadata[0].location}<br>`)
+        d3.select('#sample-metadata.card-body').append().html(`bbtype : ${jsondata.metadata[0].bbtype}<br>`)
+        d3.select('#sample-metadata.card-body').append().html(`wfreq : ${jsondata.metadata[0].wfreq}<br>`)
+    }
+    init()
    // Before creating the event shifting function, I am creating a list with all the ids using a for loop
     var list = [];
     for (i=0;i<jsondata.names.length;i++){
         list.push(jsondata.names[i])
     }
-    // The rest of the code is from ChatGPT 
+    // The codes starting with the below line are from ChatGPT. It was used to create option tags and values for the dropdown button
+    // in the html file. It caused me to remove the orginal function onchange="optionChanged(this.value)" that was inside de select tag.
+    
     // Select the <select> element with the dropdown menu
     var select = d3.select("#selDataset");
 
@@ -22,16 +66,17 @@ d3.json(url).then(function(data) {
     options.attr("value", function(d) { return d; });
   // The ChatGPT code stops here
 
-  // Now we can use a function that goes through our dropdown l
-  // first we asign a variable to our dropdown values
+  // Now we can use a function that goes through our dropdown.
+  
   function optionChanged(){
+        // first we asign a variable to our dropdown values
         let dataset = d3.select("#selDataset").property('value');
         //  The way I'm using the function is that, since the index order in my list[] is the same as the one in the json file
-        // we can find that index baed on the election value and then put it in my init function
+        // we can find that index based on the selected value and then put it in my function which is working using indexes.
         if (dataset== this.value){
             index = list.indexOf(this.value)
-            console.log(index)
-            function init() {
+            console.log(index) // just to check if the index found is correct.
+            function modify() {
                 // BAR CHART
                 let trace1 = [{
                     // we use reverse to put the chart in descending order
@@ -54,13 +99,16 @@ d3.json(url).then(function(data) {
                     },
                     text : jsondata.samples[index].otu_labels
                     }];
+                let layout =[{
+                    autosize : true
+                }]
                
                 // Displaying the plot
                 Plotly.newPlot('bar',trace1)
-                Plotly.newPlot('bubble',trace2)
+                Plotly.newPlot('bubble',trace2, layout)
         
                 //Displaying the Demographic info A.K.A metadata
-                d3.select('#sample-metadata.card-body').append().html(`id : ${jsondata.metadata[index].id}<br>`)
+                d3.select('#sample-metadata.card-body').html(`id : ${jsondata.metadata[index].id}<br>`)
                 d3.select('#sample-metadata.card-body').append().html(`ethnicity : ${jsondata.metadata[index].ethnicity}<br>`)
                 d3.select('#sample-metadata.card-body').append().html(`gender : ${jsondata.metadata[index].gender}<br>`)
                 d3.select('#sample-metadata.card-body').append().html(`age : ${jsondata.metadata[index].age}<br>`)
@@ -68,7 +116,7 @@ d3.json(url).then(function(data) {
                 d3.select('#sample-metadata.card-body').append().html(`bbtype : ${jsondata.metadata[index].bbtype}<br>`)
                 d3.select('#sample-metadata.card-body').append().html(`wfreq : ${jsondata.metadata[index].wfreq}<br>`)
             }
-            init()
+            modify()
         }
     }
     d3.select('#selDataset').on("change", optionChanged)
